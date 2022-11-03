36893679870ad6fa032e5c0f805b2d1c79465ad7
javiersanchzgil <javsan675@alu.centroafuera.es>
2022-11-03 14:20:54 +0100

    Intercambio

--- src/Intercambio.kt
+++ src/Intercambio.kt
@@ -2,8 +2,7 @@
 fun main () {
 
     var dado = (1..6).random()*10
-    var peso = 0
-    var bo = true
+
 
     val estado = arrayListOf<String>("Anciano","Adulto","Joven")
     val clases = arrayListOf<String>("Mago","Ladrón","Guerrero","Berserker","Ladron","Mercader")
@@ -13,62 +12,65 @@ fun main () {
     var raza : String = razas.random()
 
     var p1 = Personaje3("Javier","Joven","Mercader","Elfo",dado)
-    val articulos = arrayListOf<articulo>(articulo(3,50),articulo(6,30),articulo(4,70),articulo(3,70))
-    val listRatio = mutableListOf<Int>()
-    val mochila = mutableListOf<articulo>()
-    var i :Int = 0
-    var pos = 0
+
+    if (p1.clase == "Ladron") {
+        var peso = 0
+        var bo = true
+        val articulos = arrayListOf<articulo>(articulo(2, 50), articulo(5, 30), articulo(1, 70), articulo(3, 70))
+        val listRatio = mutableListOf<Int>()
+        val mochila = mutableListOf<articulo>()
+        var i: Int = 0
+        var pos = 0
 
 
-    articulos.forEach(){
-        var ratio = it.vi/it.wi
-        listRatio.add(ratio)
-    }
-
-    var max = listRatio[0]
-
-    listRatio.forEach(){
-        i++
-        if (it > max){
-            pos = i-1
+        articulos.forEach() {
+            var ratio = it.vi / it.wi
+            listRatio.add(ratio)
         }
-    }
-    mochila.add(articulos[pos])
 
-    listRatio.removeAt(pos)
-    articulos.removeAt(pos)
+        var max = listRatio[0]
 
-    do {
-
-        peso = 0
-        max = listRatio[0]
-        pos = 0
-        i = 0
-        listRatio.forEach(){
+        listRatio.forEach() {
             i++
-            if (it > max || it == max){
-                pos = i-1
+            if (it > max) {
+                pos = i - 1
             }
         }
+        mochila.add(articulos[pos])
 
-        mochila.forEach(){
-            peso = peso + it.wi
-        }
+        listRatio.removeAt(pos)
+        articulos.removeAt(pos)
 
-        if (peso + articulos[pos].wi <= 10){
-            mochila.add(articulos[pos])
-            listRatio.removeAt(pos)
-            articulos.removeAt(pos)
-        }
-        else
-            bo=false
+        do {
 
-    }while (bo)
+            peso = 0
+            max = listRatio[0]
+            pos = 0
+            i = 0
+            listRatio.forEach() {
+                i++
+                if (it > max || it == max) {
+                    pos = i - 1
+                }
+            }
 
-    println(p1.toString() + "   Robado:" + mochila)
+            mochila.forEach() {
+                peso = peso + it.wi
+            }
+
+            if (peso + articulos[pos].wi <= 10) {
+                mochila.add(articulos[pos])
+                listRatio.removeAt(pos)
+                articulos.removeAt(pos)
+            } else
+                bo = false
+
+        } while (bo)
+
+        println(p1.toString() + "   Robado:" + mochila)
 
 
-
+    }
 }
 
 class Personaje3{
