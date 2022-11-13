945469d92dcbd09c4b5d445421f3cfd001da7a12
Javier <javiersanchzgil@gmail.com>
2022-11-13 18:58:37 +0100

    Change

--- src/Intercambio.kt
+++ src/Intercambio.kt
@@ -1,49 +1,46 @@
 
 fun main () {
 
-    var dado = (1..6).random()*10
 
-    val articulos = arrayListOf<articulo>(articulo(7,50),articulo(6,30),articulo(4,70),articulo(8,70),articulo(6,40))
-    val listRatio = mutableListOf<Int>()
-    val mochila = mutableListOf<articulo>()
+    var dado = (1..6).random()*10
     var personajes = mutableListOf<Personaje3>()
     var i :Int = 0
     var pos = 0
+    val articulos = arrayListOf<articulo>(articulo(7,50),articulo(6,30),articulo(4,70),articulo(8,70),articulo(6,40))
+    val listRatio = mutableListOf<Int>()
+    val mochila1 = mutableListOf<articulo>()
     val nombres = arrayListOf<String>("Javier","Carlos","Juan")
     val estado = arrayListOf<String>("Anciano","Adulto","Joven")
     val clases = arrayListOf<String>("Mago","Ladron","Guerrero","Berserker","Ladron")
     val razas = arrayListOf<String>("Elfo", "Humano","Enano","Goblin")
-    var nombre : String = nombres.random()
-    var est : String = estado.random()
-    var clase : String = clases.random()
-    var raza : String = razas.random()
+
 
     do {
         i = 0
-        var per= Personaje3(nombre,est,clase,raza,dado)
+        dado = (1..6).random()*10
+        var per= Personaje3(nombres.random(),estado.random(),clases.random(),razas.random(),dado)
         personajes.add(per)
-        println("Desea añadir mas jugadores")
+        println("Desea añadir mas jugadores?")
         val resp = readln()
-    }while (!resp.equals("No"))
+    }while (!resp.equals("no"))
 
-    personajes.add(Personaje3(nombre,est,"Mercader",raza,0))
+    personajes.add(Personaje3(nombres.random(),estado.random(),"Mercader",razas.random(),0))
 
 
-
+    println("aaaaaaaaaaaaaaaaaaaaaaaa")
 
     personajes.forEach() {
         if (it.clase != "Mercader") {
             var peso = 0
             var bo = true
-            val articulos = arrayListOf<articulo>(articulo(2, 50), articulo(5, 30), articulo(1, 70), articulo(3, 70))
-            val listRatio = mutableListOf<Int>()
-            val mochila = mutableListOf<articulo>()
-            var i: Int = 0
-            var pos = 0
+            i = 0
+            pos = 0
+            var ratio = 0
+
 
 
             articulos.forEach() {
-                var ratio = it.vi / it.wi
+                ratio = it.vi / it.wi
                 listRatio.add(ratio)
             }
 
@@ -55,7 +52,7 @@ fun main () {
                     pos = i - 1
                 }
             }
-            mochila.add(articulos[pos])
+            mochila1.add(articulos[pos])
 
             listRatio.removeAt(pos)
             articulos.removeAt(pos)
@@ -73,22 +70,37 @@ fun main () {
                     }
                 }
 
-                mochila.forEach() {
+                mochila1.forEach() {
                     peso = peso + it.wi
                 }
-
-                if (peso + articulos[pos].wi <= 10) {
-                    mochila.add(articulos[pos])
+                var tam = it.tamMochila
+                if (peso + articulos[pos].wi <= tam) {
+                    mochila1.add(articulos[pos])
                     listRatio.removeAt(pos)
                     articulos.removeAt(pos)
                 } else
                     bo = false
-
             } while (bo)
 
-            println(it.toString() + "   Objetos Obtenidos:" + mochila)
+        it.mochila = mochila1 as ArrayList<articulo>
+
+
         }
     }
+    personajes.forEach(){
+        println(it.toString())
+    }
+
+
+    var elegir = personajes.size-1
+    var art = personajes[0].mochila.size
+
+    println("¿Qué jugador quieres que venda al mercader? Pulse un numero del 1 al " + elegir)
+    val resp = readln().toInt()
+    println("¿Que articulos quieres vender?" + personajes[resp-1].mochila)
+    println("Pulse un numero del 1 al " + art)
+    personajes[personajes.size-1].mochila.add(personajes[resp-1].mochila[art-1])
+    personajes[resp-1].mochila[art-1]
 
 
 
@@ -97,27 +109,34 @@ fun main () {
 
 }
 
+
+
+
 class Personaje3{
     var nombre: String = ""
     var estado: String = ""
     var clase : String = ""
     var raza : String = ""
-    var mochila : Int = 0
-    var monedero : Int = 0
+    var mochila = arrayListOf<articulo>()
+    var tamMochila : Int = 0
+    var monedero = mutableMapOf<Int,Int>(100 to 0, 25 to 0, 10 to 0, 5 to 0, 1 to 0)
 
-    constructor(nombre: String, estado: String, clase : String, raza: String, mochila : Int) {
+    constructor(nombre: String, estado: String, clase : String, raza: String,  tamMochila : Int) {
         this.nombre = nombre
         this.estado = estado
         this.clase = clase
         this.raza = raza
-        this.mochila = mochila
+        this.tamMochila = tamMochila
+
     }
 
     override fun toString(): String {
-        return "Personaje(nombre='$nombre', estado='$estado', clase='$clase', raza='$raza', mochila='$mochila')"
+        return "Personaje3(nombre='$nombre', estado='$estado', clase='$clase', raza='$raza', mochila='$mochila' , tamMochila=$tamMochila, monedero=$monedero)"
     }
 
 
 
-    }
+
+
+}
 
